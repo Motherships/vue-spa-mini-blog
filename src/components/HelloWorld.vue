97a1e7ed75563ded3f0a8d3a2a7abed5edb69afb
Motherships <damupka@gmail.com>
2021-10-24 16:26:30 +0300

    WIP: SCSS preproc added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,51 +8,22 @@ const count = ref(0);
 
 <template>
   <h1>{{ msg }}</h1>
+
   <router-link to="/">Go to Home</router-link>
   <router-link to="/about">Go to About</router-link>
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
 
-  <p>
-    See
-    <code>README.md</code>
-    for more information.
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code>
-    to test hot module replacement.
-  </p>
+  <div class="useless-button-wrapper">
+    <button type="button" @click="count++">count is: {{ count }}</button>
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 a {
+  display: block;
   color: #42b983;
 }
 
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
+.useless-button-wrapper {
+  padding-top: 40px;
 }
 </style>
